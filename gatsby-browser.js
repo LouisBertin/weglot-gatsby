dbd049c7b7f5da7db3199078e6e9d027607b6e5d
LouisBertin <bertin.louis7@gmail.com>
2020-06-25 15:22:45 +0200

    maybe on load

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,16 @@
-// const addScript = url => {
-//     const script = document.createElement("script")
-//     script.src = url
-//     script.async = true
-//     document.body.appendChild(script)
-// }
+const addScript = url => {
+    const script = document.createElement("script")
+    script.src = url
+    script.async = true
+    document.body.appendChild(script)
+}
   
-// export const onClientEntry = () => {
-//     window.onload = () => {
-//         addScript("https://cdn.weglot.com/weglot.min.js")
-//         if (window.Weglot) {
-//             window.Weglot.initialize({
-//                 api_key: 'wg_fe7ca09da0c30c4573b0fd2d79964daf3'
-//             })
-//          } 
-//     }
-// }   
+export const onClientEntry = () => {
+    window.onload = () => {
+        addScript("https://cdn.weglot.com/weglot.min.js")
+        addScript("https://www.multilingual-gatsby.com/script.js")
+    }
+}   
 
 // export const onRouteUpdate = () => {
 //     if (window.Weglot) {

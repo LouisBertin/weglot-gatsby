31f4cb190cc6e5a4c3971270e77f084189e5c6bd
LouisBertin <bertin.louis7@gmail.com>
2020-06-25 15:19:08 +0200

    hide browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,20 @@
-const addScript = url => {
-    const script = document.createElement("script")
-    script.src = url
-    script.async = true
-    document.body.appendChild(script)
-}
+// const addScript = url => {
+//     const script = document.createElement("script")
+//     script.src = url
+//     script.async = true
+//     document.body.appendChild(script)
+// }
   
-export const onClientEntry = () => {
-    window.onload = () => {
-        addScript("https://cdn.weglot.com/weglot.min.js")
-        if (window.Weglot) {
-            window.Weglot.initialize({
-                api_key: 'wg_fe7ca09da0c30c4573b0fd2d79964daf3'
-            })
-         } 
-    }
-}   
+// export const onClientEntry = () => {
+//     window.onload = () => {
+//         addScript("https://cdn.weglot.com/weglot.min.js")
+//         if (window.Weglot) {
+//             window.Weglot.initialize({
+//                 api_key: 'wg_fe7ca09da0c30c4573b0fd2d79964daf3'
+//             })
+//          } 
+//     }
+// }   
 
 // export const onRouteUpdate = () => {
 //     if (window.Weglot) {
